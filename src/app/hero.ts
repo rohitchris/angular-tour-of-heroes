eeea08bcd4c3a217aa37fb88ef48119456e98961
Rohit Chris D'Souza <rohit@tomorrownetworks.com>
2018-05-14 12:26:26 +0530

    Initial build

---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+
+export class Hero {
+  id: number;
+  salutation: string;
+  name: string;
+  designation: string;
+}
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,19 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroesRoro = [
+      { id: 11, salutation: 'Mr.', name: 'Nice', designation: 'Head Chef' },
+      { id: 12, salutation: 'Mr.', name: 'Narco', designation: 'Project Lead' },
+      { id: 13, salutation: 'Ms.', name: 'Bombasto', designation: 'Team Lead' },
+      { id: 14, salutation: 'Mrs.', name: 'Celeritas', designation: 'Creative Director' },
+      { id: 15, salutation: 'Ms.', name: 'Magneta', designation: 'Techie' },
+      { id: 16, salutation: 'Mr.', name: 'RubberMan', designation: 'Techie' },
+      { id: 17, salutation: 'Mr.', name: 'Dynama', designation: 'Techie' },
+      { id: 18, salutation: 'Dr.', name: 'IQ', designation: 'Techie' },
+      { id: 19, salutation: 'Mr.', name: 'Magma', designation: 'Head Chef' },
+      { id: 20, salutation: 'Mr.', name: 'Tornado', designation: 'Head Chef' }
+    ];
+    return {heroesRoro};
+  }
+}
